fbfb4e1184f083a30c4be4d440a9d0a07788d3d2
Edwin Corea <Edwin Corea>
2017-01-21 18:36:50 -0600

    SearchBar class-based component.

--- prod/ReduxSimpleStarter/src/components/search_bar.js
+++ prod/ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 
-const SearchBar = () => {
-    return <input />;
-};
+// Functional component. Not aware of its state.
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+// We need the component to be aware of its state, to communicate to ther components. 
+// Promote to class-based component.
+class SearchBar extends Component {
+    render(){
+        return <input />;
+    }                
+}
 
 
 export default SearchBar;
