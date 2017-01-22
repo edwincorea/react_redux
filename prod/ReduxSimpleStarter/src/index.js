0bfd34c6fc107a2ff8ff38463943ad951089d2f2
Edwin Corea <Edwin Corea>
2017-01-21 18:35:38 -0600

    Youtube API.  SearchBar Functional component.

--- prod/ReduxSimpleStarter/src/index.js
+++ prod/ReduxSimpleStarter/src/index.js
@@ -1,3 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyCu9-mouQ4bZUmDrkUKzL5fae1VHs5DAXA";
+
 // Create a new component. This component should produce some HTML
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 
+
+// Take this component's generated HTML and put it on the page 
+ReactDom.render(<App />, document.querySelector(".container"));
