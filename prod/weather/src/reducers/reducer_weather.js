92b1871847c2483fdb4e45ca3f38bbd41f70feea
Edwin Corea <Edwin Corea>
2017-02-05 18:37:51 -0600

    Avoid state mutations in Redux.

--- prod/weather/src/reducers/reducer_weather.js
+++ prod/weather/src/reducers/reducer_weather.js
@@ -1,4 +1,19 @@
-export default function(state = null, action){
-    
+import {FETCH_WEATHER} from "../actions/index";
+
+export default function(state = [], action){
+
+    switch (action.type){
+        case FETCH_WEATHER:
+            //NEVER mutate state
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype#Accessor_methods
+            //return state.push(action.payload.data);
+            
+            //return new array with data array added: pure function.
+            //return state.concat([action.payload.data]);
+
+            //Using ES6 spread syntax for arrays
+            return [action.payload.data, ...state];
+    }
+
     return state;
 }
