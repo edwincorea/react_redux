610321760865f3faa7416995392ba9d36f1788dd
Edwin Corea <Edwin Corea>
2017-02-12 12:38:31 -0600

    Labelling of units.

--- prod/weather/src/components/chart.js
+++ prod/weather/src/components/chart.js
@@ -1,15 +1,22 @@
+import _ from "lodash";
 import React from "react";
-import {Sparklines, SparklinesLine} from "react-sparklines";
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from "react-sparklines";
 
 //class based component or just a functional component?
 //It's gonna be just some props in, we choose functional component for chart.
+
+function average(data){
+    return _.round(_.sum(data) / data.length);
+}
 
 export default (props) => {
     return (
         <div>
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color}/>
+                <SparklinesReferenceLine type="avg" />
             </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
         </div>
     );
 };
--- prod/weather/src/containers/weather_list.js
+++ prod/weather/src/containers/weather_list.js
@@ -5,22 +5,22 @@ import Chart from "../components/chart";
 class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
-        
+
         const temperatures = cityData.list.map(weather => weather.main.temp);
-        const humidities = cityData.list.map(weather => weather.main.humidity);
         const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
                 <td>
-                    <Chart data={temperatures} color="red" />
+                    <Chart data={temperatures} color="red" units="K" />
                 </td>
                 <td>
-                    <Chart data={humidities} color="green" />
-                </td>
+                    <Chart data={pressures} color="blue" units="hPa" />
+                </td>                
                 <td>
-                    <Chart data={pressures} color="blue" />
+                    <Chart data={humidities} color="green" units="%"/>
                 </td>                
             </tr>
         );
@@ -32,9 +32,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>    
                 <tbody>
