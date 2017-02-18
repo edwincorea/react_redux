682f44727403386d5ae12bf95163f6ae7205dff8
Edwin Corea <Edwin Corea>
2017-02-18 17:14:13 -0600

    Fetching data with lifecycle methods.

--- prod/blog/src/components/posts_index.js
+++ prod/blog/src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default () => {
-    return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+    //Lifecycle methods
+    //https://facebook.github.io/react/docs/react-component.html
+    componentWillMount(){        
+        console.log("this will be a good time to call an action creator to fetch posts");
+    }
+
+    render(){
+        return (
+            <div>List of blog posts.</div>
+        );
+    }
+}
+
+export default PostsIndex;
