4341b04853c6e3787e5c4eb1b5ccafdedf7a7528
Edwin Corea <Edwin Corea>
2017-02-18 17:47:37 -0600

    Creating New Posts.

--- prod/blog/src/components/posts_index.js
+++ prod/blog/src/components/posts_index.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-//import {bindActionCreators} from "redux";
 
 import {fetchPosts} from "../actions/index";
 
@@ -18,12 +17,4 @@ class PostsIndex extends Component {
     }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     return bindActionCreators({fetchPosts}, dispatch);
-// };
-
-//mapStateToProps is null here...
-
-//export default connect(null, mapDispatchToProps)(PostsIndex);
-//export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
 export default connect(null, {fetchPosts})(PostsIndex);
