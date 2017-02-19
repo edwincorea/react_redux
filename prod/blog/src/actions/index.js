eec4b79f6c9b0cf6f64c24a42f951bad14beeb47
Edwin Corea <Edwin Corea>
2017-02-18 22:15:51 -0600

    Loading Data on Render.

--- prod/blog/src/actions/index.js
+++ prod/blog/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
+export const FETCH_POST = "FETCH_POST";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=dfsdsfdsfds";
@@ -26,3 +27,12 @@ export const createPost = (props) => {
         payload: request
     };
 };
+
+export const fetchPost = (id) => {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    };    
+}
--- prod/blog/src/components/posts_show.js
+++ prod/blog/src/components/posts_show.js
@@ -2,10 +2,20 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router";
 
+import {fetchPost} from "../actions/index";
+
 class PostsShow extends Component {
+    componentWillMount(){        
+        this.props.fetchPost(this.props.params.id);
+    }
+
     render(){
-        return <div>Show post {this.props.params.id}</div>
+        return (
+            <div>
+                
+            </div>
+        );
     }
 }
 
-export default PostsShow;
+export default connect(null, {fetchPost})(PostsShow);
