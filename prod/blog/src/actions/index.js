40ca746a99fad6dc4704d387574f1a1a0d50166a
Edwin Corea <Edwin Corea>
2017-02-18 19:08:01 -0600

    CreatePost Action Creator.

--- prod/blog/src/actions/index.js
+++ prod/blog/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=dfsdsfdsfds";
@@ -12,6 +13,16 @@ export const fetchPosts = () => {
 
     return {        
         type: FETCH_POSTS,
+        payload: request
+    };
+};
+
+//Action creator for handleSubmit.
+export const createPost = (props) => {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);    
+
+    return {
+        type: CREATE_POST,
         payload: request
     };
 };
