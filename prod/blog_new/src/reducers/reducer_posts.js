c38c876a0fd1fc76a36f4d3b440966c220a5f05b
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-04-30 17:10:14 -0600

    Implementing Posts Reducer

---
+++ prod/blog_new/src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+import { FETCH_POSTS } from "../actions/index";
+
+export default (state = {}, action) => {
+    switch(action.type){
+        case FETCH_POSTS:
+            //console.log(action.payload.data);
+            // [ post1, post2 ] => { 4: post1, 5: post2 }
+            return _.mapKeys(action.payload.data, "id");
+        default:
+            return state;
+    }
+};
