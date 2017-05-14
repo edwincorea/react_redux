4b20d86ff25b8fdb2f269da5aa1b8f45f3aca09d
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-14 17:35:19 -0600

    Creating new posts.

--- prod/blog_new/src/index.js
+++ prod/blog_new/src/index.js
@@ -6,7 +6,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
+//Components
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -20,6 +22,7 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     </BrowserRouter>
   </Provider>
