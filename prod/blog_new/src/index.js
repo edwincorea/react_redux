09a0c4a8147bf462bcf3f9d7bc6e00489782de55
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-14 17:29:11 -0600

    Redux dev extensions store configuration.

--- prod/blog_new/src/index.js
+++ prod/blog_new/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+const enhancer = composeEnhancers(
+   applyMiddleware(promise)
+);
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStore(reducers, enhancer)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
