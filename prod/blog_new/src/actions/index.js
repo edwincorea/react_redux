eb3140bef50c787e227c736c10742134fe375f9b
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-20 10:27:34 -0600

    Receiving New Posts.

--- prod/blog_new/src/actions/index.js
+++ prod/blog_new/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const FETCH_POST = "FETCH_POST";
 export const CREATE_POST = "CREATE_POST";
 
 //const ROOT_URL = "http://reduxblog.herokuapp.com/api";
@@ -18,6 +19,15 @@ export const fetchPosts = () => {
         payload: request
     };
 };
+
+export const fetchPost = (id) => {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {        
+        type: FETCH_POST,
+        payload: request
+    };    
+}
 
 export const createPost = (values, callback) => {
     const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
--- prod/blog_new/src/reducers/reducer_posts.js
+++ prod/blog_new/src/reducers/reducer_posts.js
@@ -1,12 +1,22 @@
 import _ from "lodash";
-import { FETCH_POSTS } from "../actions/index";
+import { FETCH_POSTS, FETCH_POST } from "../actions/index";
 
 export default (state = {}, action) => {
-    switch(action.type){
+    switch(action.type){        
         case FETCH_POSTS:
-            console.log(action.payload.data);
+            //console.log(action.payload.data);
             // [ post1, post2 ] => { 4: post1, 5: post2 }            
             return _.mapKeys(action.payload.data, "_id");
+        case FETCH_POST:
+            //ES5
+            // const post = action.payload.data;
+            // const newState = {...state};
+            // newState[post.id] = post;
+            // return newState;
+
+            //ES6: key interpolation            
+            return {...state, [action.payload.data.id]: action.payload.data};
+
         default:
             return state;
     }
