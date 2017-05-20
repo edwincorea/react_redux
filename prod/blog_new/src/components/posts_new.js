54ba5f978752979cfc14fd32c5883936bd934ce3
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-19 20:53:47 -0600

    Validating forms.

--- prod/blog_new/src/components/posts_new.js
+++ prod/blog_new/src/components/posts_new.js
@@ -25,8 +25,8 @@ class PostsNew extends Component {
                   component={this.renderField}
                 />
                 <Field
-                  label="Tags"
-                  name="tags"
+                  label="Categories"
+                  name="categories"
                   component={this.renderField}
                 />                
                 <Field
@@ -39,6 +39,31 @@ class PostsNew extends Component {
     }
 }
 
+//redux-form validate function. By convention: "values" parameter
+const validate = (values) => {
+    //console.log(values) -> {title: 'asdf', categories: 'asdf', content: 'asdf'}
+    const errors = {};
+
+    // Validate inputs from 'values' object
+    if (!values.title) {
+        errors.title = "Enter a title";
+    }
+    // if (values.title.length < 3) {
+    //     errors.title = "Title must be at least 3 characters!";
+    // }    
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content please";
+    }    
+
+    // If errors is empty, the form is fine to submit.
+    // If errors has *any* properties, redux form assumes form is invalid. 
+    return errors;
+}
+
 export default reduxForm({
+    validate, 
     form: "PostsNewForm"
 })(PostsNew);
