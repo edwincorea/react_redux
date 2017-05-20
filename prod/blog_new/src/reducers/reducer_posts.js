5fa96c47a8ef652449b34df65b3754a7b8ded9db
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-20 08:15:05 -0600

    Navigation through callbacks.

--- prod/blog_new/src/reducers/reducer_posts.js
+++ prod/blog_new/src/reducers/reducer_posts.js
@@ -4,9 +4,9 @@ import { FETCH_POSTS } from "../actions/index";
 export default (state = {}, action) => {
     switch(action.type){
         case FETCH_POSTS:
-            //console.log(action.payload.data);
-            // [ post1, post2 ] => { 4: post1, 5: post2 }
-            return _.mapKeys(action.payload.data, "id");
+            console.log(action.payload.data);
+            // [ post1, post2 ] => { 4: post1, 5: post2 }            
+            return _.mapKeys(action.payload.data, "_id");
         default:
             return state;
     }
